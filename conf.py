fa225a8204185dbb937a2b69628a13750343a6b3
markwork1080 <markwork1080@protonmail.com>
2026-09-20 00:36:46 +0530

    Update HTML title for Windstream email documentation

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "Alexa! How to Complete Windstream Email Login, Sign In & Account Recovery"
+html_title = "How to Log Into My Windstream Email: Windstream Webmail Login, Password Reset & Account Recovery"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
